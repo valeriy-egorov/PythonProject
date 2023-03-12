ae39540563bfeaa8b5c674ab095944bd98105936
Egorov V.V <7744081@gmail.com>
2023-03-12 18:35:16 +0300

    Многопоточность с комментариями

--- multithreading.py
+++ multithreading.py
@@ -1,32 +1,44 @@
 import time
-from threading import Thread
+import threading
+
+summ = 0.0 #Переменная доступная всем потокам
+cof_1 = 13.0
+cof_2 = 7.0
+cof_3 = 10.0
 
 
-summ = 0.0
-cof_1, cof_2 = 13.0, 7.0
-
-
-def pot(cof):
-    global summ
+def pot(cof): #Функция для оформления потока
+    global summ #Объявление глобальной переменной (доступной из тела функции
+    print(threading.current_thread().name + " - начало работы потока с параметром " + str(cof))
     for i in range(10):
-        summ = summm(summ, cof)
+        #Переменная summ изменяется потоком
+        summ = summm(summ, cof) #Увеличение значение глобальной переенной.
+    print("\n" + threading.current_thread().name + " - конец работы, summ = " + str(summ))
 
 
-def summm(summ, cof):
-    time.sleep(0.01)
+def summm(summ, cof): #Функция для вычисления суммы
+    time.sleep(0.1) #Задержка для выявления эффекта рассинхронизации доступа потоков к одной общей переменной
     return summ + cof
 
 
-print("START")
+print("Поток1 - cоздание потока")
+t_1 = threading.Thread(name="Поток1", target=pot, args=([cof_1]), daemon=True) #Daemon=True - по останову процесса потоки уничтожаются, иначе ожидание останова потоков
+print("Поток2 - cоздание потока")
+t_2 = threading.Thread(name="Поток2", target=pot, args=([cof_2]), daemon=True)
+print("Поток3 - cоздание потока")
+t_3 = threading.Thread(name="Поток3", target=pot, args=([cof_3]), daemon=True)
 
-t_1 = Thread(target=pot(cof_1), args=(), daemon=True)
-t_2 = Thread(target=pot(cof_2), args=(), daemon=True)
+t_1.start() #Запуск потока на выполнение
+t_2.start() #Запуск потока на выполнение
+t_3.start() #Запуск потока на выполнение
 
-t_1.start()
-t_2.start()
+#Ожидание останова потоков
+print()
+while t_1.is_alive() or t_2.is_alive() or t_3.is_alive(): #Проверка is_alive - жив ли поток?
+    print("ждём останова потоков. Активных потоков = " + str(threading.active_count()))
+    t_3.join(timeout=0.5) #попытка присоединиться к потоку в течении timeout - неудачное название метода!
 
-while t_1.is_alive() or t_2.is_alive():
-    t_1.join(timeout=1)
-    print("...")
 
-print(summ)
+#Конец программы
+print()
+print("Итоговое значение summ = " + str(summ))
