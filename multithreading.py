545542fe30d5be6293591335b4b70cdd9e12971c
Alexandr <67562340+The-Expert0@users.noreply.github.com>
2023-03-15 00:16:44 +0300

    Многопоточность с блокировками и комментариями

--- multithreading.py
+++ multithreading.py
@@ -5,18 +5,18 @@ summ = 0.0  # Переменная доступная всем потокам
 cof_1 = 13.0
 cof_2 = 7.0
 cof_3 = 10.0
-lock = threading.RLock()
+lock = threading.Lock()
 
 
 def pot(cof):       # Функция для оформления потока
-    lock.acquire()  # Блокировка
     global summ     # Объявление глобальной переменной (доступной из тела функции
     print(threading.current_thread().name + " - начало работы потока с параметром " + str(cof))
     for i in range(10):
         # Переменная summ изменяется потоком
+        lock.acquire()  # Блокировка
         summ = summm(summ, cof)     # Увеличение значение глобальной переенной.
+        lock.release()  # Cнятие блокировки
     print("\n" + threading.current_thread().name + " - конец работы, summ = " + str(summ))
-    lock.release()  # Cнятие блокировки
 
 
 def summm(summ, cof):   # Функция для вычисления суммы
@@ -40,8 +40,14 @@ t_3.start()     # Запуск потока на выполнение
 print()
 while t_1.is_alive() or t_2.is_alive() or t_3.is_alive(): # Проверка is_alive - жив ли поток?
     print("ждём останова потоков. Активных потоков = " + str(threading.active_count()))
-    t_3.join(timeout=0.5)   # попытка присоединиться к потоку в течении timeout - неудачное название метода!
-
+    if t_3.is_alive():
+        t_3.join(timeout=0.5)   # Попытка присоединиться к потоку в течении timeout - неудачное название метода
+    elif t_2.is_alive():
+        t_2.join(timeout=0.5)   # !Это подключение к потоку для ожидание его остановки в течении timeout!
+    elif t_1.is_alive():
+        t_1.join(timeout=0.5)   # Ожидание остановки потока
+# 
 # Конец программы
 print()
 print("Итоговое значение summ = " + str(summ))
+# 
